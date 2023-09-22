639ed2a837c8bd3c3a581acea90bb437f3f12b9b
Rosa <kosolrosa@gmail.com>
2023-09-23 06:32:17 +1000

    Updated menu toggles

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ adelaideTime = adelaideTime.split(',').join('');
 // Refreshes time every second 
 function display_c() {
     var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct()',refresh)
+    mytime=setTimeout('display_c()',refresh)
 }
 
 
 // Display local Adelaide date and time
 window.onload = function displayDate() {
     document.getElementById("date").innerHTML = adelaideTime;
+    check_time();
     display_c();
+    
 }
+
+// Loop through the entire time/date string to get the time.
+function check_time() {
+    if (parseInt(adelaideTime[16]) >= 5 || parseInt(adelaideTime[16]) < 12 ) {
+        console.log("Closed.");
+    }
+}
+
+
+// If the time is outside of business hours, display CLOSED. 
