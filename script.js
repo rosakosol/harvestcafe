14fa8a73b6019e703ef7ab53e1e0b8a4ca1e2a00
Rosa <kosolrosa@gmail.com>
2023-10-03 20:10:08 +1100

    Opening hours and socials
    Finalised business opening hours open/close sign and inserted facebook and instagram social links

--- script.js
+++ script.js
@@ -22,33 +22,52 @@ var day = adelaideTime[0] + adelaideTime[1] + adelaideTime[2];
 var hour = adelaideTime[15] + adelaideTime[16];
 const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
-// Check if public holiday
-import Holidays from "date-holidays";
-var hd = new Holidays("AU", "SA");
-hd.getHolidays(2023);
+// Adelaide public holidays 2023
+var adelaideDate = date.toLocaleDateString('en-us', {timeZone:"Australia/Adelaide", year:"numeric", month:"short", day:"numeric"});
+const adelaidePublicHolidays = [
+"Jan 1, 2023", // New Year's Day
+"Jan 2, 2023", // New Year's Day (Additional Day)
+"Jan 26, 2023", // Australia Day
+"Mar 13, 2023", // Adelaide Cup Day
+"Apr 7, 2023", // Good Friday
+"Apr 8, 2023", // Easter Saturday
+"Apr 10, 2023", // Easter Monday
+"Apr 25, 2023", // Anzac Day
+"Jun 12, 2023", // King's Birthday
+"Oct 2, 2023", // Labour Day
+"Dec 24, 2023", // Christmas Eve
+"Dec 25, 2023", // Christmas Day
+"Dec 26, 2023", // Boxing Day
+"Dec 31, 2023",]; // New Year's Eve
 
 // Check if the cafe is open
 function check_open() {
+    var openSign = document.getElementById("open-sign");
+    var closedSign = document.getElementById("closed-sign");
     // Check if public holiday
-    if (hd.isHoliday(date)) {
-        console.log("It's a public holiday.");
+    if (adelaidePublicHolidays.includes(adelaideDate)) {
+        console.log("Closed on public holidays.");
+        closedSign.classList.toggle("active");
     }
-
     // Check weekdays
     if (weekdays.includes(day)) {
         // Cafe closed before 7am and after 3pm
         if (parseInt(hour) < 7 || parseInt(hour) > 15) {
             console.log("Closed.");
+            closedSign.classList.toggle("active");
         } else {
             console.log("Open.");
+            openSign.classList.toggle("active");
         }
     // Check weekends
     } else {
         // Cafe closed before 8am and after 3pm
         if (parseInt(hour) < 8 || parseInt(hour) > 15) {
             console.log("Closed.");
+            closedSign.classList.toggle("active");
         } else {
             console.log("Open.");
+            openSign.classList.toggle("active");
         } 
     }
 }
